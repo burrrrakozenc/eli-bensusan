aeb2394aec6908b0cededdd48dd8c267df78b407
burrrrakozenc <brk.ozenc@gmail.com>
2021-03-28 20:11:08 +0300

    nav fixed

---
+++ src/pages/designed-objects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Gallery from '../components/gallery'
+
+
+const DesignedObjects = () => {
+    return (
+        <Layout>
+            <Gallery/>
+        </Layout>
+    )
+}
+
+export default DesignedObjects
---
+++ src/pages/paintings.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Gallery from '../components/paintings-gallery'
+
+
+const Paintings = () => {
+    return (
+        <Layout>
+            <Gallery/>
+        </Layout>
+    )
+}
+
+export default Paintings
